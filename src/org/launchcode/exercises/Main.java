a1a3ca41196a3cec60759eb8289f5502d1c9ed04
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-06-25 17:55:32 -0600

    Completed CHeckFIleExtension method, including an exception handler.

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,5 +1,6 @@
 package org.launchcode.exercises;
 
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Test out your Divide() function!
-        Divide(15, 0);
+        Divide(15, 1);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -17,6 +18,9 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> studentFile : studentFiles.entrySet()) {
+            System.out.println(studentFile.getKey() + " received " + CheckFileExtension(studentFile.getValue()) + " point.");
+        }
     }
 
     public static void Divide(int x, int y)
@@ -31,14 +35,26 @@ public class Main {
             System.out.println(x/y);
         }
 
-
-
-
     }
 
-//    public static int CheckFileExtension(String fileName)
-//    {
-//        // Write code here!
-//    }
+    public static int CheckFileExtension(String fileName)
+    {
+        int points = 0;
+        if (fileName == null || fileName == "") {
+            try {
+                throw new FileNotFoundException("File submitted is not a .java file.");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            points = -1;
+        } else if (fileName.endsWith(".java")) {
+            points = 1;
+        } else if (!fileName.endsWith(".java")) {
+            points = 0;
+        }
+
+        return points;
+
+    }
 
 }
